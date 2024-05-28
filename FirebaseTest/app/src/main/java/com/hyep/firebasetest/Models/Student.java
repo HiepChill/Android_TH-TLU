544b2c20579301bf38a56e7e5d95ab5c73ad18f8
hiep_chu <chumanhvuhiep03@gmail.com>
2024-05-28 07:22:59 +0700

    init bth4 firebase

---
+++ FirebaseTest/app/src/main/java/com/hyep/firebasetest/Models/Student.java
@@ -0,0 +1,17 @@
+package com.hyep.firebasetest.Models;
+
+public class Student {
+    private String id;
+    private String name;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+
+    }
+}
